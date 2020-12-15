a008c149647571fd003082db4e1710e911ca2261
Rajit Khanna <31427851+rajitkhanna@users.noreply.github.com>
2020-12-15 16:18:36 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -107,7 +107,7 @@ module.exports = function(config) {
 
     // Bring featured post to the top of the list
     // collection.sort((post, nextPost) => nextPost.featured - post.featured);
-    collection.forEach(post => console.log(post.primary_tag));
+    // collection.forEach(post => console.log(post.primary_tag));
 
     return collection;
   });
